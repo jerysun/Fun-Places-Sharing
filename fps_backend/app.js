b026f4d01da432b9eb038d3b2f7d1e8ef2bc49a9
Jerry Sun <jerysun007@hotmail.com>
2020-04-16 01:51:48 +0200

    Added Users controller

--- fps_backend/app.js
+++ fps_backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placeRoutes = require('./routes/places-routes');
+const userRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 // then it is appended by the '/' in places-routes.js
 // the c# equivalent is the attr [Route("api/places")]
 app.use('/api/places', placeRoutes); // as a middleware
+app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
   throw new HttpError('Could not find this route.', 404);
--- fps_backend/controllers/users-controller.js
+++ fps_backend/controllers/users-controller.js
@@ -1,1 +1,49 @@
+const HttpError = require('../models/http-error');
+const uuid = require('uuid/v4');
 
+const DUMMY_USERS = [
+  {
+    id: 'ul',
+    name: 'Jerry Sun',
+    email: 'test@test.com',
+    password: 'testers'
+  }
+];
+
+const getUsers = (req, res, next) => {
+  res.status(200).json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find(u => u.email === email);
+  if (hasUser) {
+    throw new HttpError('Could not create user, email already exists.', 422);
+  }
+
+   const createdUser = {
+     id: uuid(),
+     name: name,
+     email: email,
+     password: password
+   };
+
+   DUMMY_USERS.push(createdUser);
+   res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+  }
+
+  res.status(200).json({ message: 'Logged in!' });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
