fc1bf3c30e40fb152795ebc0b99a89fd0aab6bd6
Jerry Sun <jerysun007@hotmail.com>
2020-04-29 03:33:36 +0200

    added http-hook

---
+++ fps_frontend/src/shared/hooks/http-hook.js
@@ -0,0 +1,48 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(); // or useState(undefined); or useState('');
+
+  const activeHttpRequests = useRef([]);
+
+  // wrap it with useCallback to prevent sending a request from being an infinite loop
+  // so this function never gets recreated when the component that uses this hook
+  // rerenders. This function has no specific dependencies so I add an empty array as
+  // a second argument to useCallback(). Because we ensure this function never gets
+  // recreated, so won't have inefficient rerender cycles or infinite loops at worst.
+  const sendRequest = useCallback(async(url, method = 'GET', body = null, headers = {}) => {
+    setIsLoading(true);
+    const httpAbortCtrl = new AbortController();
+    activeHttpRequests.current.push(httpAbortCtrl);
+
+    try {
+      const response = await fetch(url, {
+        method,
+        body,
+        headers,
+        signal: httpAbortCtrl.signal // we can use it to cancel this http request
+      });
+
+      const responseData = await response.json();
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const clearError = () => setError(null);
+
+  // called just before component unmount
+  useEffect(() => {
+    reutrn (() => activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort()) );
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
