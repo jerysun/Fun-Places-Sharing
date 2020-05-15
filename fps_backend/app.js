219f92a509cd2c1a364e8aeeb9d72826a6bc3334
Jerry Sun <jerysun007@hotmail.com>
2020-05-15 15:19:47 +0200

    fixed a bug to adapt the hosting environment

--- fps_backend/app.js
+++ fps_backend/app.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 // places is the DB name, if it exists just open it otherwise crete then open it
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-hiwha.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
         .then(() => {
-          app.listen(5000);
+          app.listen(process.env.PORT || 5000);
         })
         .catch(err => {
           console.error(err);
